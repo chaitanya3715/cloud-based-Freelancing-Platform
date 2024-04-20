cf4a5f39084f931de9841f5e04ccf62da6838145
chaitanya3715 <166927904+chaitanya3715@users.noreply.github.com>
2024-04-21 02:08:11 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,11 @@ const app = express();
 dotenv.config({path:'./config/config.env'})
 
 app.use(
-    cors()
+    cors({
+       origin: ['http://localhost:5173'],
+      method: ["GET", "POST", "DELETE", "PUT"],
+      credentials: true,
+    })
   );
 
   app.use(cookieParser());
